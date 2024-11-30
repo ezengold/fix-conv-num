240414710d5abee5e989e9b0d4d26f595b1774c5
Ezeckiel Noudegbessi <noudekiki@gmail.com>
2024-11-30 17:00:56 +0100

    Initial Commit

---
+++ FixConvNum/FixConvNumApp.swift
@@ -0,0 +1,17 @@
+//
+//  FixConvNumApp.swift
+//  FixConvNum
+//
+//  Created by ezen on 30/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct FixConvNumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
